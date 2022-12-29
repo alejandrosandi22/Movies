30133bd7b19ae625c94ac8d6849d1096a0a787c3
alesf22 <alesf22@gmail.com>
2022-12-28 18:33:18 -0600

    change bg button color

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -113,7 +113,7 @@ export default function Hero() {
                 </button>
                 <button
                   type='button'
-                  className='bg-white border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 bg-zinc-800/40 text-white border-zinc-600 hover:bg-zinc-700 hover:border-zinc-600 focus:ring-zinc-700'
+                  className='border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 bg-zinc-800/40 text-white border-zinc-600 hover:bg-zinc-700 hover:border-zinc-600 focus:ring-zinc-700'
                 >
                   Trailer
                 </button>
@@ -164,7 +164,7 @@ export default function Hero() {
                 </button>
                 <button
                   type='button'
-                  className='bg-white border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 bg-zinc-800/40 text-white border-zinc-600 hover:bg-zinc-700 hover:border-zinc-600 focus:ring-zinc-700'
+                  className='border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 bg-zinc-800/40 text-white border-zinc-600 hover:bg-zinc-700 hover:border-zinc-600 focus:ring-zinc-700'
                 >
                   Trailer
                 </button>
